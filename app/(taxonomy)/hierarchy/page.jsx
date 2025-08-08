5bdf9d8333a548ef39711eed2f8c1bde52303cd7
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-08-08 19:10:41 +0700

    add graph

---
+++ app/(taxonomy)/hierarchy/page.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+import Hierarchy from "./hierarchy.jsx"
+
+export default function Page() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("/data/flare-2.json")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+
+  return (
+    <div>
+      <h1 className="mt-2 text-4xl font-bold tracking-tight text-slate-900 dark:text-slate-100">Evaluation Result</h1>
+      <Hierarchy data={data} />
+    </div>
+  )
+}
---
+++ app/(taxonomy)/taxonomy/page.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+import Taxonomy from "./toxonomy"
+
+export default function Page() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("/data/flare-2.json")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+
+  return (
+    <div>
+      <h1 className="mt-2 text-4xl font-bold tracking-tight text-slate-900 dark:text-slate-100">Evaluation Result (This page could be change)</h1>
+      <Taxonomy data={data} />
+    </div>
+  )
+}
